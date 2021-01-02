b035825f9418d15a148c105f982725a2828860d7
Fabio Vedovelli <vedovelli@gmail.com>
2021-01-02 14:47:54 +0100

    Módulo 4 - Aula 004 - Testar HomeController.index

--- src/controllers/home.controller.unit.spec.js
+++ src/controllers/home.controller.unit.spec.js
@@ -1,5 +1,17 @@
+import { index } from './home.controller';
+
 describe('Controllers > Home', () => {
-  it('should do something', () => {
-    expect(1).toBe(1);
+  const res = {
+    status: jest.fn(() => res),
+    json: jest.fn(() => res),
+  };
+
+  it('should do something', async () => {
+    await index(null, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ welcome: 'Welcome Stranger!' });
   });
 });
